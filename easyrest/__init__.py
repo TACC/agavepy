96183795c2e2bb0d3c69c39e020f2f5c96a3aec9
Walter Moreira <walter@waltermoreira.net>
2014-05-12 10:19:12 -0500

    Initial commit

---
+++ easyrest/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Walter Moreira'
+__email__ = 'walter@waltermoreira.net'
+__version__ = '0.1.0'
---
+++ easyrest/easyrest.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+EasyRest
+========
+
+Very basic wrapper for requests to play with the Agave API.
+
+"""
+
+import os
+import urllib
+
+import requests
+
+
+def verb(verb_name):
+    """Construct an HTTP verb.
+
+    Save the response as `last_response`, for debugging purposes.
+
+    """
+    def _verb(self, *args, **kwargs):
+        kwargs['auth'] = self.auth
+        fun = getattr(requests, verb_name)
+        resp = fun(*args, **kwargs)
+        self.last_response = resp
+        return resp.json()
+    return _verb
+
+
+class AgaveAPI(object):
+
+    BASE = 'https://agave.iplantc.org'
+
+    def __init__(self, user, password, tenant=None):
+        self.user = user
+        self.password = password
+        self.tenant = tenant or self.BASE
+        self.auth = requests.auth.HTTPBasicAuth(self.user, self.password)
+
+    def _url(self, *args):
+        return urllib.parse.urljoin(self.BASE, os.path.join(*args))
+
+    GET = verb('get')
+    POST = verb('post')
+
+    def clients_create(self, client_name):
+        url = self._url('clients/v2')
+        data = {'clientName': client_name,
+                'tier': 'Unlimited'}
+        return self.POST(url, data=data)
+
+    def clients_list(self):
+        url = self._url('clients/v2')
+        return self.GET(url)
