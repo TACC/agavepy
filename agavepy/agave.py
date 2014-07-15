0e69be57081f2ce0f176974dd4058f99d66ad463
Walter Moreira <walter@waltermoreira.net>
2014-07-15 14:08:21 -0500

    Add setter to attribute/dictionary

--- agavepy/agave.py
+++ agavepy/agave.py
@@ -168,6 +168,9 @@ class AttrDict(dict):
     def __getattr__(self, key):
         return self[key]
 
+    def __setattr__(self, key, value):
+        self[key] = value
+
 
 class AgaveProcessor(SwaggerProcessor):
 
