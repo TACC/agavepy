5b3731804f34662e4abcddc4230483d0163689ca
Joe Stubbs <joestubbs5@gmail.com>
2016-02-06 11:02:13 -0600

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requires = [pkg for pkg in open('requirements.txt').readlines()]
 
 setup(
     name='agavepy',
-    version='0.3.3',
+    version='0.3.4',
     description='SDK for Agave',
     long_description=readme,
     author='Joe Stubbs, Walter Moreira',
