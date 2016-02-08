8981cd5c3128a6594fd27d2f0cc44b6d4b10ca75
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-02-08 17:58:02 -0600

    Update test suite framework. Tests are now passing.

--- agavepy/tests/test_agave.py
+++ agavepy/tests/test_agave.py
@@ -1,3 +1,14 @@
+# agavepy test suite.
+#
+# To run the tests:
+# 1. create a file, test_credentials.json, in the tests directory with credentials for accessing a tenant (see the ex).
+#    Make sure to update: storage, storage_key, execution and execution_key and app_name
+#
+# 2. update the test-storage.json, test-compute.json, test-app.json and test-job.json
+#
+# 3. run with py.test from the cwd.
+#
+
 import datetime
 import json
 import os
@@ -50,9 +61,40 @@ def validate_app(app):
     assert type(app.revision) is int and app.revision > 0
     assert app.version
 
-def test_raise_exception(agave):
-    with pytest.raises(a.AgaveException):
-        agave.systems.get(systemId='foo')
+def validate_client(client):
+    pass
+    # todo - this should turned back on when the clients service is working again.
+    # assert client.consumerKey
+
+def validate_file(file):
+    assert file.format
+    assert type(file.lastModified) is datetime.datetime
+    assert type(file.length) is int
+    assert file.mimeType
+    assert file.name
+    assert file.path
+    assert file.system
+
+def validate_job(job):
+    assert job.appId
+    if 'endTime' in job:
+        assert job.endTime is None or  type(job.endTime) is datetime.datetime
+    assert job.executionSystem
+    assert job.id
+    assert job.name
+    assert job.owner
+    if 'startTime' in job:
+        assert job.startTime is None or type(job.startTime) is datetime.datetime
+    assert job.status
+
+def validate_system(system):
+    assert type(system.public) is bool
+    assert system.name
+    assert system.status
+    assert system.type
+
+def validate_profile(prof, user):
+    assert prof.username == user
 
 def test_add_compute_system(agave, test_compute_system):
     system = agave.systems.add(body=test_compute_system)
@@ -63,7 +105,7 @@ def test_add_compute_system(agave, test_compute_system):
                            headers={'Authorization': 'Bearer ' + agave.token.token_info['access_token']},
                            verify=agave.verify)
     except requests.exceptions.HTTPError as exc:
-        print "Error trying to set compute system as default:", str(exc)
+        print "Error trying to register compute system:", str(exc)
         raise exc
     validate_system(system)
 
@@ -79,7 +121,6 @@ def test_add_storage_system(agave, test_storage_system):
         print "Error trying to set storage system as default:", str(exc)
         raise exc
     validate_system(system)
-
 
 def test_list_apps(agave):
     apps = agave.apps.list()
@@ -102,39 +143,15 @@ def test_add_app(agave, test_app):
     app = agave.apps.add(body=test_app)
     validate_app(app)
 
-def validate_client(client):
-    assert client.consumerKey
-
 def test_list_clients(agave):
     clients = agave.clients.list()
     for client in clients:
         validate_client(client)
 
-def validate_file(file):
-    assert file.format
-    assert type(file.lastModified) is datetime.datetime
-    assert type(file.length) is int
-    assert file.mimeType
-    assert file.name
-    assert file.path
-    assert file.system
-
-def test_list_files(agave, credentials):
-    files = agave.files.listOnDefaultSystem(filePath=credentials['username'])
+def test_list_files(agave, credentials, test_storage_system):
+    files = agave.files.list(filePath=credentials['storage_user'], systemId=credentials['storage'])
     for file in files:
         validate_file(file)
-
-def validate_job(job):
-    assert job.appId
-    if 'endTime' in job:
-        assert job.endTime is None or  type(job.endTime) is datetime.datetime
-    assert job.executionSystem
-    assert job.id
-    assert job.name
-    assert job.owner
-    if 'startTime' in job:
-        assert job.startTime is None or type(job.startTime) is datetime.datetime
-    assert job.status
 
 def test_list_jobs(agave):
     jobs = agave.jobs.list()
@@ -145,11 +162,13 @@ def test_submit_job(agave, test_job):
     job = agave.jobs.submit(body=test_job)
     validate_job(job)
 
-def validate_system(system):
-    assert type(system.public) is bool
-    assert system.name
-    assert system.status
-    assert system.type
+def test_get_profile(agave, credentials):
+    prof = agave.profiles.get()
+    validate_profile(prof, credentials['username'])
+
+def test_list_profiles(agave, credentials):
+    prof = agave.profiles.listByUsername(username='me')
+    validate_profile(prof, credentials['username'])
 
 def test_list_systems(agave):
     systems = agave.systems.list()
@@ -157,13 +176,13 @@ def test_list_systems(agave):
         validate_system(system)
 
 def test_list_public_systems(agave):
-    systems = agave.systems.list(publicOnly=True)
+    systems = agave.systems.list(public=True)
     for system in systems:
         validate_system(system)
         assert system.public
 
 def test_list_private_systems(agave):
-    systems = agave.systems.list(privateOnly=True)
+    systems = agave.systems.list(public=False)
     for system in systems:
         validate_system(system)
         assert not system.public
@@ -172,7 +191,7 @@ def test_list_default_systems(agave):
     systems = agave.systems.list(default=True)
     for system in systems:
         validate_system(system)
-        assert not system.get('default')
+        assert system.get('default')
 
 def test_token_access(agave, credentials):
     token = agave.token.refresh()
--- agavepy/tests/testdata.py
+++ agavepy/tests/testdata.py
@@ -19,154 +19,35 @@ class TestData(object):
         """
         Example storage system read from an external file.
         """
-        storage = self.file_to_json('test-storage-eod.json')
+        storage = self.file_to_json('test-storage.json')
         storage['id'] = self.local_data['storage']
-        storage['storage']['auth']['password'] = self.local_data['storage_password']
+        storage['storage']['auth']['privateKey'] = self.local_data['storage_key']
         return storage
 
     def get_test_compute_system(self):
         """
         Example compute system defined inline.
         """
-        compute = self.file_to_json('test-compute-eod.json')
+        compute = self.file_to_json('test-compute.json')
         compute['id'] = self.local_data['execution']
-        compute['login']['auth']['password'] = self.local_data['execution_password']
-        compute['storage']['auth']['password'] = self.local_data['storage_password']
+        compute['login']['auth']['privateKey'] = self.local_data['execution_key']
+        compute['storage']['auth']['privateKey'] = self.local_data['storage_key']
         return compute
 
-    def get_test_app(self):
-        """
-        Example application defined inline.
-        """
-        test_app = a.AttrDict()
-        #Whether the application is available.
-        test_app.available = True
-        #Whether the application supports checkpointing.
-        test_app.checkpointable = False # bool
-        #The max execution time that should be used if none is given in a job description. Ignore if the system does not support schedulers.
-        test_app.defaultMaxRunTime = None # str
-        #The default memory in GB to pass to the scheduler if none is given in the job description. This must be less than the max memory parameter in the target queue definition.
-        test_app.defaultMemory = 1 # str
-        #The number of nodes that should be used if none is given in a job description. Ignore if the system does not support schedulers.
-        test_app.defaultNodeCount = None # str
-        #The number of processors to pass to the scheduler if none are given in the job description. This must be 1 if the app is serial.
-        test_app.defaultProcessors = 1 # str
-        #The queue on the execution system that should be used if none is given in a job description. Ignore if the system does not support schedulers.
-        test_app.defaultQueue = None # str
-        #The location in the user's default storage system containing the application wrapper and dependencies.
-        test_app.deploymentPath = self.local_data['deployment_path'] # str
-        #The system id of the storage system where this app should run.
-        test_app.deploymentSystem = self.local_data['storage'] # str
-        #The system id of the execution system where this app should run.
-        test_app.executionSystem = self.local_data['execution'] # str
-        #The execution type of the application. If you're unsure, it's probably HPC.
-        test_app.executionType = 'HPC' # str
-        #The URL where users can go for more information about the app.
-        test_app.helpURI = 'http://www.gnu.org/s/coreutils/manual/html_node/wc-invocation.html' # str
-        #The icon to associate with this app.
-        test_app.icon = None # str
-
-        #The inputs files for this application. -- list[ApplicationInput]
-        test_app.inputs = [
-              {
-                 "id":"query1",
-                 "value":{
-                    "default":"agave://demo.storage.example.com/apps/wc-1.00/picksumipsum.txt",
-                    "validator":"",
-                    "required":True,
-                    "visible":True
-                 },
-                 "details":{
-                    "label":"File to count words in:",
-                    "description":""
-                 },
-                 "semantics":{
-                    "ontology":[
-                       "http://sswapmeet.sswap.info/util/TextDocument"
-                    ],
-                    "minCardinality":1,
-                    "maxCardinality":1,
-                    "fileTypes":[
-                       "text-0"
-                    ]
-                 }
-              }
-           ]
-        #The label to use when generating forms.
-        test_app.label = 'Word Count' # str
-        #The full text description of this input to use when generating forms.
-        test_app.longDescription = '' # str
-        #An array of modules to load prior to the execution of the application.
-        test_app.modules = ["purge", "load TACC"] # list[str]
-        #The name of the application. The name does not have to be unique, but the combination of name and version does.
-        test_app.name = self.local_data['app_name'] # str
-        #An array of ontology values describing this application.
-        test_app.ontology = ["http://sswapmeet.sswap.info/algorithms/wc"] # list[str]
-        #The outputs files for this application.
-        test_app.outputs = None # list[ApplicationOutput]
-        #The parallelism type of the application. If you're unsure, it's probably SERIAL.
-        test_app.parallelism = 'SERIAL' # str
-
-        #The inputs parameters for this application - list[ApplicationParameter]
-        test_app.parameters =  [
-              {
-                 "id":"printLongestLine",
-                 "value":{
-                    "default":False,
-                    "type":"string",
-                    "validator":"",
-                    "visible":True,
-                    "required":False
-                 },
-                 "details":{
-                    "label":"Print the length of the longest line",
-                    "description":"Command option -L"
-                 },
-                 "semantics":{
-                    "ontology":[
-                       "xs:boolean"
-                    ]
-                 }
-              }
-           ]
-        #The short description of this application.
-        test_app.shortDescription = 'Counts words in a file' # str
-        #An array of tags related to this application.
-        test_app.tags = ["textutils", "gnu"] # list[str]
-        #The path to the wrapper script relative to the deploymentPath.
-        test_app.templatePath = 'wrapper.sh' # str
-        #The path to the test script relative to the deploymentPath.
-        test_app.testPath = 'wrapper.sh' # str
-        #The version of the application in #.#.# format. While the version does not need to be unique, the combination of name and version does have to be unique.
-        test_app.version = '1.00' # str
-
-        return test_app
-
     def get_test_app_from_file(self):
-        app = self.file_to_json('test-app-eod.json')
-        app['name'] += self.local_data['app_name'] + '_'
+        app = self.file_to_json('test-app.json')
+        app['name'] = self.local_data['app_name']
         app['executionSystem'] = self.local_data['execution']
         app['deploymentSystem'] = self.local_data['storage']
         return app
-
-
-    def get_test_job(self):
-        """
-        Example job defined inline.
-        """
-        test_job = a.AttrDict()
-        test_job.name = self.local_data['app_name']
-        test_job.appId = '{}-{}'.format(self.local_data['app_name'],
-                                        self.local_data['app_version'])
-        test_job.memoryPerNode = 1
-        test_job.inputs = {"query1":"agave://data.iplantcollaborative.org/jstubbs/5kB.txt"}
-        return test_job
 
     def get_test_job_from_file(self):
         """
         Example job request read in from an external file.
         """
         test_app = self.get_test_app_from_file()
-        job = self.file_to_json('test-job-eod.json')
+        test_stor = self.get_test_storage_system()
+        job = self.file_to_json('test-job.json')
         job['appId'] = test_app['name'] + '-' + test_app['version']
+        job['inputs']['wf'] = "agave://{}//data/algebra.yml".format(test_stor['id'])
         return job
