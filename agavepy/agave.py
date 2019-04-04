11b1e342f79bec4bd3bf05935ecf927bc0e7134f
Christian R. Garcia <christian.g21@gmail.com>
2019-04-04 13:35:24 -0500

    Updated resources loader to take seperate files
    Created an updateDict function to take in many dictionaries and update
    them recursively, including the list of dictionaries inside the 'apis'
    key and in some other places. Note: This updating of list inside of
    dictionaries works by appending list, thus dictionaries inside of list
    will merely be added to the list, not checked for repeats or updated
    values.
    
    Also split resources.json.j2 into different files now located in the
    /resources folder inside of /agavepy/. The 'apis' key inside the
    resources was split into 13 seperate files based on the 13 values the
    'apis'dictionary had; they are listed by their route. Additionally items
    not in the 'apis' key were all placed in the misc.json.j2 file inside of
    agavepy/resources/.

--- agavepy/agave.py
+++ agavepy/agave.py
@@ -55,12 +55,45 @@ def load_resource(api_server):
     :type api_server: str
     :rtype: dict
     """
-    conf = ConfigGen('resources.json.j2')
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(HERE),
-                             trim_blocks=True, lstrip_blocks=True)
-    rsrcs = json.loads(conf.compile(
-        {'api_server_base': urllib.parse.urlparse(api_server).netloc}, env))
+    rsrcs = {}
+    rsrc_files = ['resources/misc.json.j2',
+                  'resources/api_clients.json.j2',
+                  'resources/api_apps.json.j2',
+                  'resources/api_files.json.j2',
+                  'resources/api_jobs.json.j2',
+                  'resources/api_meta.json.j2',
+                  'resources/api_monitors.json.j2',
+                  'resources/api_notifications.json.j2',
+                  'resources/api_postits.json.j2',
+                  'resources/api_profiles.json.j2',
+                  'resources/api_systems.json.j2',
+                  'resources/api_transforms.json.j2',
+                  'resources/api_actors.json.j2',
+                  'resources/api_admin.json.j2']
+    for rsrc_file in rsrc_files:
+        conf = ConfigGen(rsrc_file)
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(HERE),
+                                 trim_blocks=True, lstrip_blocks=True)
+
+        new_rsrcs = json.loads(conf.compile(
+            {'api_server_base': urllib.parse.urlparse(api_server).netloc}, env))
+        updateDict(rsrcs, new_rsrcs)
     return rsrcs
+
+
+def updateDict(base_dict, new_dict):
+    for key, val in new_dict.items():
+        if isinstance(val, dict):
+            base_dict[key] = updateDict(base_dict.get(key, {}), val)
+        elif isinstance(val, list):
+            if not key in base_dict:
+                base_dict[key] = []
+            for list_dict in val:
+                base_dict[key].append(list_dict)
+        else:
+            base_dict[key] = val
+    return base_dict
+
 
 def with_refresh(client, f, *args, **kwargs):
     """Call function ``f`` and refresh token if needed."""
