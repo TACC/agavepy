8999a35d62e5163909488a14320e5bb2b148be50
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-09-19 15:06:25 -0500

    Added packages

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ data_files = [('', ['requirements.txt', 'requirements-py2.txt'])]
 
 setup(
     name='agavepy',
-    version='1.0.0',
+    version='1.0.0a1',
     description='SDK for TACC Tapis (formerly Agave)',
     long_description=readme,
     author='Texas Advanced Computing Center',
@@ -47,7 +47,9 @@ setup(
     url='https://github.com/TACC/agavepy',
     packages=[
         'agavepy',
-        'agavepy.swaggerpy'
+        'agavepy.swaggerpy',
+        'agavepy.settings',
+        'agavepy.tenants'
     ],
     package_dir={'agavepy': 'agavepy'},
     package_data={'agavepy': [
