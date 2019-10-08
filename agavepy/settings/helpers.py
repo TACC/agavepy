3a967ad17e77ebea45bd77d5e7982d9d9d208a26
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-08 10:45:43 -0500

    Added missing 'n' to recognized Boolean string values

--- agavepy/settings/helpers.py
+++ agavepy/settings/helpers.py
@@ -3,7 +3,7 @@ import os
 ENV_PREFIX = 'TAPIS_PY'
 
 BOOLEAN_TRUE_STRINGS = ('true', 'on', 'ok', 'y', 'yes', '1')
-BOOLEAN_FALSE_STRINGS = ('no', 'false', 'off', '0', 'none')
+BOOLEAN_FALSE_STRINGS = ('false', 'off', 'n', 'no', '0')
 
 __all__ = [
     'ns_os_environ_get', 'fix_assets_path', 'array_from_string',
