4945fa098820cc623ee9d1e6dea359c554187609
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-10 16:10:53 -0500

    Created a specific configuration for TAPIS_DEFAULT_TENANT_ID. This is just used to pre-populate the tenant option when running auth init in the CLI

--- agavepy/settings/tenancy.py
+++ agavepy/settings/tenancy.py
@@ -2,7 +2,8 @@ import os
 from agavepy.constants import (ENV_TENANTS_URL,
                                ENV_TENANT_ID,
                                DEFAULT_TENANTS_URL,
-                               DEFAULT_TENANT_ID)
+                               DEFAULT_TENANT_ID,
+                               ENV_DEFAULT_TENANT_ID)
 
 __all__ = ['TAPIS_TENANTS_URL', 'TAPIS_DEFAULT_TENANT_ID']
 
@@ -10,4 +11,4 @@ TAPIS_TENANTS_URL = os.environ.get(
     ENV_TENANTS_URL, DEFAULT_TENANTS_URL)
 
 TAPIS_DEFAULT_TENANT_ID = os.environ.get(
-    ENV_TENANT_ID, DEFAULT_TENANT_ID)
+    ENV_DEFAULT_TENANT_ID, DEFAULT_TENANT_ID)
