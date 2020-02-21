af8bee27cc8ef0d2799dbecd31da6cf09daf699e
Matt Vaughn <vaughn@tacc.utexas.edu>
2020-02-21 15:21:28 -0600

    Imports

--- agavepy/__init__.py
+++ agavepy/__init__.py
@@ -6,3 +6,5 @@ from . import tenants
 import sys  # noqa
 from agavepy import asynchronous  # noqa
 sys.modules['agavepy.async'] = asynchronous  # noqa
+from agavepy import interactive  # noqa
+from agavepy import settings  # noqa
